c88b12d43e04ce5843b7b55bd784c44d0c439f56
nickelsey <nicholas.elsey@gmail.com>
2017-08-02 15:10:08 -0400

    updated model name for KNN

--- jet_playground/models/test_models.in.py
+++ jet_playground/models/test_models.in.py
@@ -146,7 +146,7 @@ def main( args ):
           print( "Training KNN models")
           best_knn_model = knn.train_knn( X_train_prepared, y_train, n_jobs=3 )
           knn_predictions = np.array(best_knn_model.predict( X_test_prepared )).squeeze()
-          tools.compare_model_to_geant( y_test['reco_pt'], geant=X_test['pt'], model=knn_predictions, model_name="Decision Tree", n_jobs=max_jobs )
+          tools.compare_model_to_geant( y_test['reco_pt'], geant=X_test['pt'], model=knn_predictions, model_name="KNN", n_jobs=max_jobs )
           df_out['knn_pt'] = knn_predictions
 
       ## write file out
