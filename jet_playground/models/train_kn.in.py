78d0de1550e807eee08e359578565dc2565c004a
nickelsey <nicholas.elsey@gmail.com>
2017-08-02 22:50:02 -0400

    removed gridsearch parameters that dont exist for KNN

--- jet_playground/models/train_kn.in.py
+++ jet_playground/models/train_kn.in.py
@@ -3,8 +3,8 @@ from sklearn.model_selection import GridSearchCV
 import numpy as np
 
 ''' grid search for a KNN regressor '''
-def train_knn( X, y, n_neighbors=[1, 2, 3, 5, 10, 15, 20], weights=[ "uniform", "distance" ], random_state=[420], verbose=1, n_jobs=1 ):
-  param_grid = [ { 'n_neighbors' : n_neighbors, 'weights' : weights, 'random_state' : random_state } ]
+def train_knn( X, y, n_neighbors=[1, 2, 3, 5, 10, 15, 20], weights=[ "uniform", "distance" ], verbose=1, n_jobs=1 ):
+  param_grid = [ { 'n_neighbors' : n_neighbors, 'weights' : weights  ]
   knn_reg = KNeighborsRegressor()
   grid_search = GridSearchCV(knn_reg, param_grid, cv=5, scoring='neg_mean_squared_error', verbose=verbose, n_jobs=n_jobs)
   grid_search.fit( X, y )
